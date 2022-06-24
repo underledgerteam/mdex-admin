a92bb5dbb1a45200ecf7b0827055feaac090159f
Possawee jab <62610796+PM-Jab@users.noreply.github.com>
2022-06-24 23:23:43 +0700

    ✨ feature: connect wallet, get account address, and get current balance(ether)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => {
   return (
     <div>
-      <h1 className="text-3xl">
+      <h1 className="text-3xl bg-black text-white">
         Multisig Wallet
       </h1>
     </div>
--- src/components/WalletCard.tsx
+++ src/components/WalletCard.tsx
@@ -1,52 +1,65 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { ethers } from "ethers";
 
-declare var window: any // telling the TypeScript compiler to treat window as of type any hence ignore any warnings.
+declare var window: any; // telling the TypeScript compiler to treat window as of type any hence ignore any warnings.
 
 const WalletCard = () => {
 
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [defaultAccount, setDefaultAccount] = useState(null);
-  const [userBalance, setUserBalance] = useState(null);
-  const [connectButtonText, setConnectButtonText] = useState('Connect Wallet')
-  const {ethereum} = window;
+  const [defaultAccount, setDefaultAccount] = useState('');
+  const [userBalance, setUserBalance] = useState('');
+  const [errorMessage, setErrorMessage] = useState(''); // the inital state value can't be null
+  const { ethereum } = window;
 
   const connectWallet = async () => {
-    alert("Connect to Metamask!!")
-    if(ethereum){
+    alert("Connect to Metamask!!");
+    if (ethereum) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
-      // const account = await provider.send("eth_requestAccounts", []);
       const accounts = await ethereum.request({
         method: "eth_requestAccounts",
       });
 
-      setDefaultAccount(accounts[0]);
+      changeAccountHandler(accounts[0]);
+
+    } else {
+      setErrorMessage('Please install MetaMask'); //can't assign to parameter of type 'SetStateAction<null or undefine>'
     }
+
+  }
+
+  const changeAccountHandler = (newAccount: string) : void =>  {
+    setDefaultAccount(newAccount);
+    getUserBalance(newAccount);
+  }
+
+  const getUserBalance = async (address: string) => {
+    const balance = await ethereum.request({
+      method: 'eth_getBalance', 
+      params: [address, 'latest']
+    });
+
+    setUserBalance(ethers.utils.formatEther(balance));
   }
 
   return (
     <div className="bg-sky-500 flex flex-col w-full justify-center items-center">
-      <button 
-        type="button"
-        onClick={connectWallet}
-        className="flex flex-row justify-center item-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+      {!defaultAccount && (
+        <button
+          type="button"
+          onClick={connectWallet}
+          className="flex flex-row justify-center item-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
         >
-          <p className="text-white text-base font-semibold">{connectButtonText}</p>
-      </button>
+          <p className="text-white text-base font-semibold">Connect Wallet</p>
+        </button>
+      )}
 
       <div className="flex flex-col">
-        <p>
-          Address: {defaultAccount}
-        </p>
-        <p>
-          Balance: {userBalance}
-        </p>
+        <p>Address: {defaultAccount}</p>
+        <p>Balance: {userBalance}</p>
         {errorMessage}
       </div>
-      
     </div>
   );
-}
+};
 
 export default WalletCard;
