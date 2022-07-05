7d395ec84ee6c64ce29aeeef065a3eb1a9e0f7b6
Kaseres <arenas.2533@gmail.com>
2022-07-06 03:31:51 +0700

    :wrench: :construction: #36 Change Navbar

--- src/navigation/Navbar.tsx
+++ src/navigation/Navbar.tsx
@@ -1,15 +1,34 @@
-import React,{useState,useContext} from "react";
+import { useContext } from "react";
 import { AdminContext } from "../context/AdminContext";
+import { shortenAddress } from "../utils/shortenAddress.util";
+
+// Assets
+import MdexLogo from "../assets/images/logo/mdex_logo.png";
 
 const Navbar = () => {
   const admin = useContext(AdminContext);
-  return (
-    <div>
-      <nav className="w-full flex justify-between items-center p-4 bg-blue-500 text-white">
-        <div className="text-3xl">Mdex-Admin</div>
-        <div className="text-xl">{admin?.adminAccount}</div>
-      </nav>
+  
+  return (<div className="flex flex-col">
+  <div className="navbar bg-custom-navbar w-full lg:mx-auto justify-between items-center font-bold lg:px-8 py-3">
+    <div className="navbar-start">
+      
+      <div className="w-14">
+        <button className="text-xl flex text-2xl text-white uppercase font-bold cursor-pointer"><img src={MdexLogo} /></button>
+      </div>
     </div>
+    <div className="lg:flex navbar-end">
+    {admin?.adminAccount ? (
+      <div
+        id="walletAddress"
+        className="inline-block text-xl px-4 py-2 leading-none border rounded-lg text-white mx-2 lg:mt-0 cursor-pointer"
+        onClick={() => { }}
+      >
+        {shortenAddress(admin?.adminAccount)}
+      </div>
+      ) : ( <></> )}
+    </div>
+  </div>
+</div>
   );
 };
 
