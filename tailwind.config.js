a7d0db873d48d89295de9e77b1589b91f390434d
Kaseres <arenas.2533@gmail.com>
2022-07-06 03:27:38 +0700

    :package: add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,46 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ["Open Sans", "sans-serif"],
+        body: ["Open Sans", "sans-serif"],
+      },
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out'
+      },
+      visibility: ["group-hover"],
+      colors: {
+        custom: {
+          navbar: "#232533",
+          cardBg: "#101118",
+          black: "#0E0F15",
+          blue1: "#0088FF",
+          blue2: "#0F9AFF",
+          yellow1: "#FDD231",
+          yellow2: "#F9C200",
+          gray1: "#C5CAF6",
+          gray2: "#C4C8ED",
+          purple1: "#A259FF",
+          purple2: "#8E00FD"
+        },
+      },
+      dropShadow: {
+        'navbar': '0 4px 4px rgba(0, 0, 0, 0.25)',
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
