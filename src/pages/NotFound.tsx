8f2d4947d80d4df32927bbedcd65f3165628bd2c
Kaseres <arenas.2533@gmail.com>
2022-07-06 11:24:48 +0700

    :wrench: :construction: #29 add layout page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+const NotFound: FC = () => {
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Page not found.</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
