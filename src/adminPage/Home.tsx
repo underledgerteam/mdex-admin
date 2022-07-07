254e841c31f558b728ba6c05b3a262ffda137e2d
Kaseres <arenas.2533@gmail.com>
2022-07-07 23:59:40 +0700

    :wrench: :construction: #29 Home select network

--- src/adminPage/Home.tsx
+++ src/adminPage/Home.tsx
@@ -5,8 +5,15 @@ import { AdminContext } from "../context/AdminContext";
 import Transactions from "../components/Transactions";
 import Card from "../components/shared/Card";
 
+import InputSelectNetwork from "../components/shared/InputSelectNetwork";
+import { SWAP_CONTRACTS } from "../utils/constants";
+
 const Home = () => {
   const admin = useContext(AdminContext);
+
+  const listOptionNetwork = Object.keys(SWAP_CONTRACTS).map((key) => {
+    return { value: key, label: (<Fragment><img className="mask mask-squircle mr-1" src="https://placeimg.com/160/160/arch" width={30} /> {SWAP_CONTRACTS[Number(key)].NETWORK_SHORT_NAME}</Fragment>) };
+  });
 
   return (
     <Card 
@@ -16,7 +23,14 @@ const Home = () => {
       <Fragment>
         <div className="w-full h-screen text-center">
         {admin?.adminAccount ? (
+          <>
+          <InputSelectNetwork 
+            listOption={listOptionNetwork}
+            selectionUpdate={"Select Network"}
+            selectLabel="Select Network"
+          />
           <Transactions />
+          </>
         ) : ( 
           <ButtonConnect />
         )}
--- src/context/AdminContext.tsx
+++ src/context/AdminContext.tsx
@@ -1,15 +1,19 @@
 import React, { useEffect, useState, createContext } from "react";
 import { ethers } from "ethers";
+import { SUPPORT_CHAIN } from "../utils/constants";
+
+import { SWAP_CONTRACTS } from "../utils/constants"
 
 declare var window: any; // telling the TypeScript compiler to treat window as of type any hence ignore any warnings.
 
 const { ethereum } = window;
 
 type AdminContextProviderType = {
-  adminAccount: string
-  errorMessage: string
-  connectWallet: () => Promise<void>
-  checkIfWalletIsConnected: () => Promise<void>
+  adminAccount: string;
+  errorMessage: string;
+  connectWallet: () => Promise<void>;
+  checkIfWalletIsConnected: () => Promise<void>;
+  walletSwitchChain: (chainId: number) => void;
 };
 
 type AdminContextProviderProps = {
@@ -22,6 +26,7 @@ export const AdminContextProvider = ({
   children,
 }: AdminContextProviderProps) => {
   const [adminAccount, setAdminAccount] = useState<string>("");
+  const [isSupported, setIsSupported] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
 
   const changeHandler = (account: string): void => {
@@ -49,11 +54,14 @@ export const AdminContextProvider = ({
   const checkIfWalletIsConnected = async (): Promise<void> => {
     try {
       if (!ethereum) return alert("Please install metamask");
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const { chainId } = await provider.getNetwork();
 
       const accounts = await ethereum.request({ method: "eth_accounts" });
 
       if (accounts.length) {
         setAdminAccount(accounts[0]);
+        setIsSupported(SUPPORT_CHAIN.includes(chainId));
       } else {
         console.log("No accounts found");
       }
@@ -62,6 +70,44 @@ export const AdminContextProvider = ({
       console.log(error);
 
       throw new Error("No ethereum object.");
+    }
+  };
+  
+  const walletAddChain = async (chainId: number): Promise<void> => {
+    try {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      await provider.send("wallet_addEthereumChain", [{
+        chainId: ethers.utils.hexValue(Number(chainId)),
+        chainName: SWAP_CONTRACTS[chainId].NETWORK_NAME,
+        nativeCurrency: {
+          name: SWAP_CONTRACTS[chainId].NATIVE_CURRENCY!.NAME,
+          symbol: SWAP_CONTRACTS[chainId].NATIVE_CURRENCY!.SYMBOL,
+          decimals: SWAP_CONTRACTS[chainId].NATIVE_CURRENCY!.DECIMALS,
+        },
+        rpcUrls: SWAP_CONTRACTS[chainId].RPC_URLS,
+        blockExplorerUrls: SWAP_CONTRACTS[chainId].BLOCK_EXPLORER_URLS,
+      }]);
+    } catch (error) {
+      throw new Error(`Can't Add ${SWAP_CONTRACTS[chainId].NETWORK_NAME} to Wallet`);
+    }
+  };
+  const currentNetwork = async() => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const { chainId } = await provider.getNetwork();
+    return chainId;
+  };
+  const walletSwitchChain = async (chainId: number): Promise<void> => {
+    try {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const currentChain = await currentNetwork();
+      if (chainId !== currentChain) {
+        await provider.send("wallet_switchEthereumChain", [{ chainId: ethers.utils.hexValue(chainId) }]);
+      }
+    } catch (error: any) {
+      if (error.code === 4902) {
+        await walletAddChain(chainId);
+      }
+      throw new Error("Can't Switch Chain");
     }
   };
 
@@ -73,7 +119,7 @@ export const AdminContextProvider = ({
   }, [adminAccount]);
 
   return (
-    <AdminContext.Provider value={{ adminAccount, errorMessage, connectWallet, checkIfWalletIsConnected }}>
+    <AdminContext.Provider value={{ adminAccount, errorMessage, connectWallet, checkIfWalletIsConnected, walletSwitchChain }}>
       {children}
     </AdminContext.Provider>
   );
