4da01d6739c820ca6e854cde98adced4a2b60461
Kaseres <arenas.2533@gmail.com>
2022-07-20 09:54:51 +0700

    🔧🔀  #29 change function Balance

--- src/context/action.context.tsx
+++ src/context/action.context.tsx
@@ -1,15 +1,8 @@
-import { useState, useEffect, createContext, useContext } from "react";
+import { useState, useEffect, createContext } from "react";
 import { ethers } from "ethers";
 
 // context
-import { AdminContext } from "./AdminContext";
 import { ActionContextInterface, ActionProviderInterface } from "../types/contexts/action.context";
-
-// ABI
-import CALL_ABI from '../utils/CallToken.json';
-
-// constants
-import { MULTI_SIG_WALLET_CONTRACTS, TEST_TOKEN_ADDRESS, MULTI_SIG_DECIMAL_SET } from "../utils/constants";
 
 declare var window: any;
 
@@ -24,34 +17,7 @@ export const ActionContext = createContext<ActionContextInterface>(defaultValue)
 
 export const ActionProvider = ({ children }: ActionProviderInterface) => {
 
-  const admin = useContext(AdminContext);
-
   const [balance, setBalance] = useState(0);
-
-
-  const getMultiSigBalance = async() => {
-    try {
-      const provider = new ethers.providers.Web3Provider(ethereum);
-      const { chainId } = await provider.getNetwork();
-
-      const testTokenContract = new ethers.Contract(TEST_TOKEN_ADDRESS, CALL_ABI, provider);
-      const tokenBalance = await testTokenContract.balanceOf(MULTI_SIG_WALLET_CONTRACTS[chainId].ADDRESS);
-      const tokenUnits = await testTokenContract.decimals();
-      const tokenBalanceInEther = ethers.utils.formatUnits(tokenBalance, tokenUnits);
-
-      setBalance(Number(parseFloat(tokenBalanceInEther).toFixed(MULTI_SIG_DECIMAL_SET)));
-      
-    } catch (error) {
-      // console.log(error);
-    }
-  }
-
-  useEffect(() => {
-    const init = async () => {
-      await getMultiSigBalance();
-    };
-    init();
-  },[]);
 
 
   return (
