753e12c5f81549e6079a90396fcfc5e8d0c83d86
Mob <tammanoon24@gmail.com>
2022-07-21 17:06:29 +0700

    :construction: #40 add key to row
    clear warning

--- src/components/Transactions.tsx
+++ src/components/Transactions.tsx
@@ -1,12 +1,12 @@
-import { useContext } from "react";
+import { FC, useContext } from "react";
 import { ActionContext } from "../context/action.context";
 import { AdminContext } from "src/context/AdminContext";
 import { shortenAddress } from "../utils/shortenAddress.util";
 
-const Transactions = () => {
-  const { transaction, voteConfirmTransaction, voteNotConfirmTransaction } =
-    useContext(ActionContext);
+const Transactions: FC = () => {
+  const { transaction, voteConfirmTransaction, voteNotConfirmTransaction } = useContext(ActionContext);
   const admin = useContext(AdminContext);
+
   return (
     <div className="mt-10">
       <div className="overflow-x-auto">
@@ -27,7 +27,7 @@ const Transactions = () => {
             {transaction.length > 1 ? (
               transaction.map((transactions) => {
                 return (
-                  <tr className="text-center">
+                  <tr key={`${transactions.id}`} className="text-center">
                     <td>{transactions.id}</td>
                     <td>{shortenAddress(transactions.caller)}</td>
                     <td>{shortenAddress(transactions.to)}</td>
@@ -40,25 +40,24 @@ const Transactions = () => {
                         transactions.status === "WAITING" ?
                           (admin?.adminAccount != transactions.caller.toLowerCase() && (
                             <>
-                              <button className="btn mx-2" onClick={() => {voteConfirmTransaction(Number(transactions.id))}}>Yes</button>
-                              <button className="btn mx-2" onClick={() => {voteNotConfirmTransaction(Number(transactions.id))}}>No</button>
+                              <button className="btn mx-2" onClick={() => { voteConfirmTransaction(Number(transactions.id)); }}>Yes</button>
+                              <button className="btn mx-2" onClick={() => { voteNotConfirmTransaction(Number(transactions.id)); }}>No</button>
                             </>
-                          )):
+                          )) :
                           transactions.status === "READY" ?
                             (admin?.adminAccount != transactions.caller.toLowerCase() && (
                               <>
                                 <button className="btn">Execute</button>
                               </>
-                            )):
+                            )) :
                             transactions.status === "QUEUE" ?
                               (<>
                                 <button className="btn">Cancel</button>
-                              </>):
+                              </>) :
                               transactions.status === "FAIL" ?
                                 ("Fail") :
-                                  transactions.status === "SUCCESS" ?
-                                    ("Success") : ("Error")
-      
+                                transactions.status === "SUCCESS" ?
+                                  ("Success") : ("Error")
                       }
                     </td>
                   </tr>
