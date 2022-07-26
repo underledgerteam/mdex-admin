2deb272f7400938fc2ec7f97792022a38e5185e3
Possawee jab <62610796+PM-Jab@users.noreply.github.com>
2022-07-26 10:29:42 +0700

    🚧 #70 check notify

--- src/context/action.context.tsx
+++ src/context/action.context.tsx
@@ -1,17 +1,24 @@
 import { useState, useEffect, createContext, useContext } from "react";
 import { ethers, utils } from "ethers";
 import dayjs from "dayjs";
-import { useNotifier } from 'react-headless-notifier';
+import { useNotifier } from "react-headless-notifier";
 // context
 import { AdminContext } from "./AdminContext";
 import {
   ActionContextInterface,
   ActionProviderInterface,
-  TransactionInterface
+  TransactionInterface,
 } from "../types/contexts/action.context";
 // constants
-import { MULTI_SIG_WALLET_CONTRACTS, MULTI_SIG_DECIMAL_SET, TRANSACTION_STATUS } from "../utils/constants";
-import { SuccessNotification, DangerNotification } from "../components/shared/Notification";
+import {
+  MULTI_SIG_WALLET_CONTRACTS,
+  MULTI_SIG_DECIMAL_SET,
+  TRANSACTION_STATUS,
+} from "../utils/constants";
+import {
+  SuccessNotification,
+  DangerNotification,
+} from "../components/shared/Notification";
 
 declare var window: any;
 const { ethereum } = window;
@@ -19,15 +26,16 @@ const { ethereum } = window;
 const defaultValue: ActionContextInterface = {
   transactions: [],
   treasuryBalance: 0,
-  getTransactions: async () => { },
-  submitTransaction: async (to: string, value: number) => { },
-  voteConfirmTransaction: async () => { },
-  voteNotConfirmTransaction: async () => { },
-  cancelTransaction: async () => { },
-  executeTransaction: async () => { }
+  getTransactions: async () => {},
+  submitTransaction: async (to: string, value: number) => {},
+  voteConfirmTransaction: async () => {},
+  voteNotConfirmTransaction: async () => {},
+  cancelTransaction: async () => {},
+  executeTransaction: async () => {},
 };
 
-export const ActionContext = createContext<ActionContextInterface>(defaultValue);
+export const ActionContext =
+  createContext<ActionContextInterface>(defaultValue);
 
 export const ActionProvider = ({ children }: ActionProviderInterface) => {
   const admin = useContext(AdminContext);
@@ -42,11 +50,18 @@ export const ActionProvider = ({ children }: ActionProviderInterface) => {
 
       const multiSigContract = MULTI_SIG_WALLET_CONTRACTS[chainId];
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(multiSigContract.ADDRESS, multiSigContract.ABI, signer);
+      const contract = new ethers.Contract(
+        multiSigContract.ADDRESS,
+        multiSigContract.ABI,
+        signer
+      );
       const response = await contract.getBalance();
       const treasuryBalanceInEther = ethers.utils.formatUnits(response);
-      setTreasuryBalance(Number(parseFloat(treasuryBalanceInEther).toFixed(MULTI_SIG_DECIMAL_SET)));
-
+      setTreasuryBalance(
+        Number(
+          parseFloat(treasuryBalanceInEther).toFixed(MULTI_SIG_DECIMAL_SET)
+        )
+      );
     } catch (error) {
       console.log(error);
     }
@@ -64,8 +79,15 @@ export const ActionProvider = ({ children }: ActionProviderInterface) => {
         signer
       );
       let transactions = await multisigContract.getTransactions();
-      const isVotedArray = await Promise.all(transactions.map((txn: { id: number; }) => multisigContract.isConfirmed(txn.id, admin?.adminAccount)));
-      transactions = transactions.map((txn: any, index: number) => ({ ...txn, isVoted: isVotedArray[index] }));
+      const isVotedArray = await Promise.all(
+        transactions.map((txn: { id: number }) =>
+          multisigContract.isConfirmed(txn.id, admin?.adminAccount)
+        )
+      );
+      transactions = transactions.map((txn: any, index: number) => ({
+        ...txn,
+        isVoted: isVotedArray[index],
+      }));
       setTransactions(normalizedTransaction(transactions));
     } catch (error) {
       console.error("GetTransaction", error);
@@ -82,10 +104,12 @@ export const ActionProvider = ({ children }: ActionProviderInterface) => {
         caller: txn.caller,
         to: txn.to,
         value: utils.formatEther(txn.value),
-        timestamp: dayjs.unix(ethers.BigNumber.from(txn.timestamp).toNumber()).format("DD/MM/YYYY"),
+        timestamp: dayjs
+          .unix(ethers.BigNumber.from(txn.timestamp).toNumber())
+          .format("DD/MM/YYYY"),
         status: TRANSACTION_STATUS[txn.status],
-        vote: (countYes + countNo),
-        isVoted: txn.isVoted
+        vote: countYes + countNo,
+        isVoted: txn.isVoted,
       });
     }
     return arrayTransactions;
@@ -106,43 +130,51 @@ export const ActionProvider = ({ children }: ActionProviderInterface) => {
         to,
         ethers.utils.parseEther(value.toString())
       );
+      notify(<SuccessNotification message={"Submit Success"} />);
     } catch (error: any) {
-      notify(<DangerNotification
-        message={error.message}
-      />);
+      notify(<DangerNotification message={error.message} />);
     }
   };
 
-  const voteConfirmTransaction = async (transactionId: number): Promise<void> => {
+  const voteConfirmTransaction = async (
+    transactionId: number
+  ): Promise<void> => {
     try {
       const provider = new ethers.providers.Web3Provider(ethereum);
       const { chainId } = await provider.getNetwork();
 
       const multiSigContract = MULTI_SIG_WALLET_CONTRACTS[chainId];
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(multiSigContract.ADDRESS, multiSigContract.ABI, signer);
+      const contract = new ethers.Contract(
+        multiSigContract.ADDRESS,
+        multiSigContract.ABI,
+        signer
+      );
       await contract.confirmTransaction(transactionId, { gasLimit: 100000 });
-      notify(<SuccessNotification
-        message={"You vote success"}
-      />);
+      notify(<SuccessNotification message={"You vote success"} />);
     } catch (error: any) {
-      notify(<DangerNotification
-        message={error.message}
-      />);
+      notify(<DangerNotification message={error.message} />);
     }
   };
 
-  const voteNotConfirmTransaction = async (transactionId: number): Promise<void> => {
+  const voteNotConfirmTransaction = async (
+    transactionId: number
+  ): Promise<void> => {
     try {
       const provider = new ethers.providers.Web3Provider(ethereum);
       const { chainId } = await provider.getNetwork();
 
       const multiSigContract = MULTI_SIG_WALLET_CONTRACTS[chainId];
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(multiSigContract.ADDRESS, multiSigContract.ABI, signer);
+      const contract = new ethers.Contract(
+        multiSigContract.ADDRESS,
+        multiSigContract.ABI,
+        signer
+      );
       await contract.noConfirmTransaction(transactionId, { gasLimit: 100000 });
-    } catch (error) {
-      console.log(error);
+      notify(<SuccessNotification message={"You vote no success"} />);
+    } catch (error: any) {
+      notify(<DangerNotification message={error.message} />);
     }
   };
 
@@ -153,10 +185,15 @@ export const ActionProvider = ({ children }: ActionProviderInterface) => {
 
       const multiSigContract = MULTI_SIG_WALLET_CONTRACTS[chainId];
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(multiSigContract.ADDRESS, multiSigContract.ABI, signer);
+      const contract = new ethers.Contract(
+        multiSigContract.ADDRESS,
+        multiSigContract.ABI,
+        signer
+      );
       await contract.cancelTransaction(transactionId, { gasLimit: 100000 });
-    } catch (error) {
-      console.log(error);
+      notify(<SuccessNotification message={"You cancel success"} />);
+    } catch (error: any) {
+      notify(<DangerNotification message={error.message} />);
     }
   };
 
@@ -167,10 +204,15 @@ export const ActionProvider = ({ children }: ActionProviderInterface) => {
 
       const multiSigContract = MULTI_SIG_WALLET_CONTRACTS[chainId];
       const signer = provider.getSigner();
-      const contract = new ethers.Contract(multiSigContract.ADDRESS, multiSigContract.ABI, signer);
+      const contract = new ethers.Contract(
+        multiSigContract.ADDRESS,
+        multiSigContract.ABI,
+        signer
+      );
       await contract.executeTransaction(transactionId, { gasLimit: 100000 });
-    } catch (error) {
-      console.log(error);
+      notify(<SuccessNotification message={"Transaction success"} />); // scan link
+    } catch (error: any) {
+      notify(<DangerNotification message={error.message} />);
     }
   };
 
@@ -194,7 +236,7 @@ export const ActionProvider = ({ children }: ActionProviderInterface) => {
         voteConfirmTransaction,
         voteNotConfirmTransaction,
         cancelTransaction,
-        executeTransaction
+        executeTransaction,
       }}
     >
       {children}
